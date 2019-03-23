3eec6f7df013abc4891c0de8e6e9b4b84a1efe40
Xingyi Cai <caixingyi@tamu.edu>
2019-03-23 23:22:03 +0000

    edit users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Just begin to cut your cord, #{@user.username}!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update 
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was update successfully."
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_190409) do
+ActiveRecord::Schema.define(version: 2019_03_23_230824) do
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_03_23_190409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "password_digest"
   end
 
 end
